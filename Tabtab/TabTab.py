830dbf8c7b0e56708b5787ab9e21373da0432b0a
DrawingProcess <csj000714@khu.ac.kr>
2021-05-12 11:03:59 +0900

    tabtab mini

---
+++ Tabtab/TabTab.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://main-gpt2-large-jeong-hyun-su.endpoint.ainize.ai/gpt2-large/long"
+data = {
+    "text": "bitcoin is", 
+    "num_samples": 5,
+    "length": 20
+}
+
+response = requests.post(url, data=data)
+
+if response.status_code == 200:
+    res = response.json() 
+    print(res)
+else:
+	print("Failed Request")
